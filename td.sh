aef791bbde8e7c766e5eed0527c7d6a73e4a6340
Pedro Manse <pedro.manse@dmk3.com.br>
2024-11-07 18:08:53 -0300

    really want a deezer downloader instead of having to relly on yt

--- td.sh
+++
@@ -1,7 +0,0 @@
-./yt-dlp_linux -x -o "videos/%(title)s - %(uploader)s.%(ext)s"
-
-# Köppen As Fuck - Chris Christodoulou @ LBs6fU8s8vw
-#  {"name": "Köppen As Fuck", "id": "LBs6fU8s8vw", "by": "Chris Christodoulou - Topic"},
-# She don't want to live - I Hate My Self Because @ Hc10THfOcNs
-#	{"name": "She don't want to live", "id": "Hc10THfOcNs", "by": "I Hate My Self Because"},
-
